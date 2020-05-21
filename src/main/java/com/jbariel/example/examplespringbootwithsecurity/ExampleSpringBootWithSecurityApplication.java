67edc5e59c41754ac7110edee20b3909307a243b
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-21 23:21:29 +0000

    base project from start.spring.io

---
+++ src/main/java/com/jbariel/example/examplespringbootwithsecurity/ExampleSpringBootWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jbariel.example.examplespringbootwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootWithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbariel/example/examplespringbootwithsecurity/ExampleSpringBootWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbariel.example.examplespringbootwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootWithSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
