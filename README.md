0318a17c7edd4f852b517641ffa34603527b7812
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-22 02:06:16 +0000

    license, readme, sec adapter

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot + Spring Security
+Simple example project to show Basic Auth using spring security.
+
+# License
+Apache 2.0
--- src/main/java/com/jbariel/example/examplespringbootwithsecurity/HelloController.java
+++ src/main/java/com/jbariel/example/examplespringbootwithsecurity/HelloController.java
@@ -1,5 +1,7 @@
 package com.jbariel.example.examplespringbootwithsecurity;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/tweet")
+    @GetMapping("/canary")
     public String canary() {
         return "Tweet";
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/")
+    public String coolKid() {
+        return "Hi cool kid, you authed!";
+    }
+
+    // @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
+    // @RolesAllowed("ROLE_ADMIN")
+    @GetMapping("/admin")
+    public String reallyCoolKid() {
+        return "Hi you cool admin creature you!";
     }
 }
